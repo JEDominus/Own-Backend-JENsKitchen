5b3a184d26c29b863f44d6d319a9821584e4a8ae
JEDominus <je.dominus.hatred@gmail.com>
2022-09-04 16:52:51 -0500

    Initial commit - Let's Rock!

---
+++ src/test/java/com/jens/kitchen/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jens.kitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
