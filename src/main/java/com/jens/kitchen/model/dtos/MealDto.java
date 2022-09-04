418ca540dbf9a036b627669c23d94a30d6e6a6f1
JEDominus <je.dominus.hatred@gmail.com>
2022-09-04 17:39:46 -0500

    JENs-1 ... MealDto class fixed

--- src/main/java/com/jens/kitchen/model/dtos/MealDto.java
+++ src/main/java/com/jens/kitchen/model/dtos/MealDto.java
@@ -1,9 +1,19 @@
 package com.jens.kitchen.model.dtos;
 
 import com.jens.kitchen.model.enums.TypesEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 import java.util.List;
 
+@Data
+@Builder
+@Accessors(chain = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class MealDto {
     private String id;
     private String mealName;
