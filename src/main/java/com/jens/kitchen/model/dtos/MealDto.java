0788d11f56cfdf194e691c3c272e75760a0d3116
JEDominus <je.dominus.hatred@gmail.com>
2022-09-04 17:38:14 -0500

    JENs-1 ... Classes structure updated

--- src/main/java/com/jens/kitchen/model/dtos/MealDto.java
+++ src/main/java/com/jens/kitchen/model/dtos/MealDto.java
@@ -1,20 +1,11 @@
 package com.jens.kitchen.model.dtos;
 
 import com.jens.kitchen.model.enums.TypesEnum;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.experimental.Accessors;
 
 import java.util.List;
 
-@Data
-@Builder
-@Accessors(chain = true)
-@AllArgsConstructor
-@NoArgsConstructor
 public class MealDto {
+    private String id;
     private String mealName;
     private TypesEnum mealType;
     private List<IngredientDto> ingredients;
